5866f78211f85987a7e6d9f935016cc40e0aa0c9
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-04-04 14:51:16 -0400

    set up testing

--- docxology_test.go
+++ docxology_test.go
@@ -11,5 +11,29 @@ func TestMain(m *testing.M) {
 }
 
 func TestDocxOnDiscUnzip(t *testing.T) {
+	//
+}
 
+func TestExtractLocalFile(t *testing.T) {
+	//
+}
+
+func TestExtractFileHTTP(t *testing.T) {
+	//
+}
+
+func TestMapFiles(t *testing.T) {
+	//
+}
+
+func TestCopyToOS(t *testing.T) {
+	//
+}
+
+func TestFindDoc(t *testing.T) {
+	//
+}
+
+func TestXMLExtractText(t *testing.T) {
+	//
 }
